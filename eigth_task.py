186c51c74bc88e8966a58dc809373815ba6d7184
Zenin Nikita <n.zenin@g.nsu.ru>
2024-12-01 23:30:39 +0700

    edit

--- eigth_task.py
+++ eigth_task.py
@@ -1,126 +1,109 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import solve
 
-def f_explicit(t, u, v):
-    """Функция для вычисления производных u и v."""
-    du = 998 * u + 1998 * v
-    dv = -999 * u - 1999 * v
-    return du, dv
 
-def explicit_euler(f, t0, u0, v0, t_end, h):
-    """Явная схема Эйлера."""
-    t_values = np.arange(t0, t_end + h, h)
-    u_values = np.zeros_like(t_values)
-    v_values = np.zeros_like(t_values)
+def f_u(u, v):
+    return 998 * u + 1998 * v
 
-    u_values[0], v_values[0] = u0, v0
+def f_v(u, v):
+    return -999 * u - 1999 * v
 
-    for i in range(1, len(t_values)):
-        du, dv = f(t_values[i - 1], u_values[i - 1], v_values[i - 1])
-        u_values[i] = u_values[i - 1] + h * du
-        v_values[i] = v_values[i - 1] + h * dv
 
-    return t_values, u_values, v_values
+u0 = -3
+v0 = 5
 
-def trapezoidal_rule(f, t0, u0, v0, t_end, h):
-    """схема трапеций."""
-    t_values = np.arange(t0, t_end + h, h)
-    u_values = np.zeros_like(t_values)
-    v_values = np.zeros_like(t_values)
 
-    u_values[0], v_values[0] = u0, v0
+def exact_solution(points, t_end):
+    time = np.linspace(0, t_end, points + 1)
+    u = 2 * (u0 + v0) * np.exp(-time) + (-u0 - 2 * v0) * np.exp(-1000 * time)
+    v = - (u0 + v0) * np.exp(-time) - (-u0 - 2 * v0) * np.exp(-1000 * time)
+    return time, u, v
 
-    for i in range(1, len(t_values)):
-        du_old, dv_old = f(t_values[i - 1], u_values[i - 1], v_values[i - 1])
-        u_guess = u_values[i - 1] + h * du_old
-        v_guess = v_values[i - 1] + h * dv_old
 
-        du_new, dv_new = f(t_values[i], u_guess, v_guess)
-        u_values[i] = u_values[i - 1] + h / 2 * (du_old + du_new)
-        v_values[i] = v_values[i - 1] + h / 2 * (dv_old + dv_new)
+def explicit_euler(u0, v0, time):
+    points = len(time) - 1
+    h = time[1] - time[0]
+    u = np.zeros(points + 1)
+    v = np.zeros(points + 1)
+    u[0], v[0] = u0, v0
+    for n in range(points):
+        u[n + 1] = u[n] + h * (998 * u[n] + 1998 * v[n])
+        v[n + 1] = v[n] + h * (-999 * u[n] - 1999 * v[n])
+    return u, v
 
-    return t_values, u_values, v_values
 
-def implicit_euler(f, t0, u0, v0, t_end, h):
-    """Неявная схема Эйлера."""
-    t_values = np.arange(t0, t_end + h, h)
-    u_values = np.zeros_like(t_values)
-    v_values = np.zeros_like(t_values)
+def implicit_euler(u0, v0, time):
+    points = len(time) - 1
+    h = time[1] - time[0]
+    u = np.zeros(points + 1)
+    v = np.zeros(points + 1)
+    u[0], v[0] = u0, v0
+    for n in range(points):
+        A = np.array([[1 - h * 998, -h * 1998], [h * 999, 1 + h * 1999]])
+        b = np.array([u[n], v[n]])
+        u_next, v_next = np.linalg.solve(A, b)
+        u[n + 1], v[n + 1] = u_next, v_next
+    return u, v
 
-    u_values[0], v_values[0] = u0, v0
 
-    A = np.array([[1 - h * 998, -h * 1998],
-                  [h * 999, 1 + h * 1999]])
+def runge_kutta(u0, v0, time):
+    points = len(time) - 1
+    h = time[1] - time[0]
+    u = np.zeros(points + 1)
+    v = np.zeros(points + 1)
+    u[0], v[0] = u0, v0
+    for n in range(points):
+        u_star = u[n] + h * f_u(u[n], v[n])
+        v_star = v[n] + h * f_v(u[n], v[n])
+        u[n + 1] = u[n] + h / 2 * (f_u(u[n], v[n]) + f_u(u_star, v_star))
+        v[n + 1] = v[n] + h / 2 * (f_v(u[n], v[n]) + f_v(u_star, v_star))
+    return u, v
 
-    for i in range(1, len(t_values)):
-        u_old, v_old = u_values[i - 1], v_values[i - 1]
-        b = np.array([u_old, v_old])
-        u_new, v_new = solve(A, b)
-        u_values[i], v_values[i] = u_new, v_new
 
-    return t_values, u_values, v_values
+t_end = 1
+grid_points = [1000, 10000, 100000, 1000000]
 
-t0 = 0
-u0 = 1
-v0 = 0
-t_end = 0.05
-h = 0.001
+error_explicit = []
+error_implicit = []
+error_rk = []
 
-# t, u_explicit, v_explicit = explicit_euler(f_explicit, t0, u0, v0, t_end, h)
-# _, u_trap, v_trap = trapezoidal_rule(f_explicit, t0, u0, v0, t_end, h)
-# _, u_implicit, v_implicit = implicit_euler(f_explicit, t0, u0, v0, t_end, h)
+for points in grid_points:
+    time, u_analytical, v_analytical = exact_solution(points, t_end)
 
-def plot_phase_trajectories(u_values_list, v_values_list, labels):
+    u_values1, v_values1 = explicit_euler(u0, v0, time)
+    u_values2, v_values2 = implicit_euler(u0, v0, time)
+    u_values3, v_values3 = runge_kutta(u0, v0, time)
+
+    error_explicit_u = np.abs(u_analytical - u_values1)
+    error_implicit_u = np.abs(u_analytical - u_values2)
+    error_rk_u = np.abs(u_analytical - u_values3)
+
+    error_explicit.append(np.max(error_explicit_u))
+    error_implicit.append(np.max(error_implicit_u))
+    error_rk.append(np.max(error_rk_u))
+
     plt.figure(figsize=(10, 6))
-    for u_values, v_values, label in zip(u_values_list, v_values_list, labels):
-        plt.plot(u_values, v_values, label=label)
-    plt.xlabel('u')
-    plt.ylabel('v')
-    plt.title('Фазовые траектории')
-    plt.legend()
+    plt.plot(time, u_analytical, ':', label='Точное решение u', color='blue')
+    plt.plot(time, u_values1, '-', label='Явный метод Эйлера u', color='red')
+    plt.plot(time, u_values2, '--', label='Неявный метод Эйлера u', color='purple')
+    plt.plot(time, u_values3, '-.', label='Полунеявный метод u', color='cyan')
+    plt.xlabel('t')
+    plt.ylabel('Значение функции')
+    plt.title(f'Решения методов при {points} точках')
+    plt.legend(loc='upper right')
     plt.grid(True)
     plt.show()
 
 
-# plot_phase_trajectories(
-#     [u_explicit, u_trap, u_implicit],
-#     [v_explicit, v_trap, v_implicit],
-#     ['Явная схема Эйлера', 'Полунеявная схема трапеций', 'Неявная схема Эйлера']
-# )
-
-def plot_time_series(t_values_list, u_values_list, v_values_list, labels):
-    plt.figure(figsize=(10, 6))
-    for t_values, u_values, v_values, label in zip(t_values_list, u_values_list, v_values_list, labels):
-        plt.plot(t_values, u_values, label=f'u ({label})', linestyle='--')
-        plt.xlabel('t')
-        plt.ylabel('u, v')
-        plt.title('Численное решение')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-        plt.plot(t_values, v_values, label=f'v ({label})', linestyle='--')
-        plt.xlabel('t')
-        plt.ylabel('u, v')
-        plt.title('Численное решение')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-
-
-
-
-step_sizes = [0.001, 0.005, 0.01, 0.06, 0.1]
-# нарисовать для разных шагов по времени
-for h in step_sizes:
-    t, u_explicit, v_explicit = explicit_euler(f_explicit, t0, u0, v0, t_end, h)
-    _, u_trap, v_trap = trapezoidal_rule(f_explicit, t0, u0, v0, t_end, h)
-    _, u_implicit, v_implicit = implicit_euler(f_explicit, t0, u0, v0, t_end, h)
-
-    plot_time_series(
-        [t, t, t],
-        [u_explicit, u_trap, u_implicit],
-        [v_explicit, v_trap, v_implicit],
-        [f'Явная схема {h}', f'Полунеявная схема {h}', f'Неявная схема {h}']
-    )
+plt.figure(figsize=(10, 6))
+plt.plot(grid_points, error_explicit, label='Явный метод Эйлера', color='blue')
+plt.plot(grid_points, error_implicit, label='Неявный метод Эйлера', color='red')
+plt.plot(grid_points, error_rk, label='Полунеявный метод', color='green')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Количество шагов (по логарифмической шкале)')
+plt.ylabel('Ошибка (по логарифмической шкале)')
+plt.title('Ошибка численных методов относительно аналитического решения')
+plt.legend()
+plt.grid(True)
+plt.show()
--- twelve_task.py
+++ twelve_task.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft, fftfreq
 from scipy.signal import windows
 
+def get_freq_array(length, spacing_rate):
+    results = np.zeros(length, int)
+    N = (length - 1) // 2 + 1
+    results[:N] = np.arange(0, N, dtype=int)
+    results[N:] = np.arange(-(length // 2), 0, dtype=int)
+    return results / (length * spacing_rate)
+
+def DFT_slow(x):
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
 a0 = 1
 a1 = 0.002
 w0 = 5.1
@@ -11,17 +26,16 @@ T = 2 * np.pi
 N = 1000
 t = np.linspace(0, T, N)
 
-
 f_t = a0 * np.sin(w0 * t) + a1 * np.sin(w1 * t)
 
 rect_window = np.ones(N)
 
 hanning_window = windows.hann(N)
 
-spectrum_rect = fft(f_t )
-frequencies = fftfreq(N, 1 / N)
+spectrum_rect = DFT_slow(f_t)
+frequencies = get_freq_array(N, 1 / N)
 
-spectrum_hanning = fft(f_t * hanning_window)
+spectrum_hanning = DFT_slow(f_t * hanning_window)
 
 power_spectrum_rect = np.abs(spectrum_rect)
 power_spectrum_hanning = np.abs(spectrum_hanning)
@@ -31,19 +45,17 @@ mask = frequencies > 0
 plt.figure(figsize=(12, 6))
 
 plt.subplot(1, 2, 1)
-plt.plot(frequencies[mask], power_spectrum_rect[mask], label='Прямоугольное окно', color='blue')
+plt.plot(frequencies[mask], power_spectrum_rect[mask] / (T), label='Прямоугольное окно', color='blue')
 plt.title('Прямоугольное окно')
 plt.xlabel('Частота [Гц]')
 plt.ylabel('Мощность')
-plt.xlim(0, 30)
 plt.grid(True)
 
 plt.subplot(1, 2, 2)
-plt.plot(frequencies[mask], power_spectrum_hanning[mask], label='Окно Ханна', color='green')
+plt.plot(frequencies[mask], power_spectrum_hanning[mask] / (T), label='Окно Ханна', color='green')
 plt.title('Окно Ханна')
 plt.xlabel('Частота [Гц]')
 plt.ylabel('Мощность')
-plt.xlim(0, 30)
 plt.grid(True)
 
 plt.tight_layout()
